a8cce1be120fe7dfdf3d66b134db3a5cad9a72d2
Jacob Moore <mooreja2@flip2.engr.oregonstate.edu>
2022-04-27 14:30:02 -0700

    Taking grader's suggestions and making artist-albums and artists-songs M:N

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,3 @@
-
-
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -7,134 +5,160 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Table `Users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Users` ;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(255) NOT NULL,
   `last_name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NULL,
   `date_joined` DATETIME NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`user_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`playlist`
+-- Table `Playlists`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`playlist` ;
+DROP TABLE IF EXISTS `Playlists` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`playlist` (
-  `id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `Playlists` (
+  `playlist_id` INT NOT NULL,
   `created_date` DATETIME NOT NULL,
   `playlist_name` VARCHAR(2555) NOT NULL,
   `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`playlist_id`),
   INDEX `fk_playlist_user_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_playlist_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`user` (`id`)
+    REFERENCES `Users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`artist`
+-- Table `Albums`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`artist` ;
+DROP TABLE IF EXISTS `Albums` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`artist` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  `bio` VARCHAR(255) NOT NULL,
-  `genre` VARCHAR(255) NOT NULL,
-  `label` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`album`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`album` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`album` (
-  `id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `Albums` (
+  `album_id` INT NOT NULL,
   `title` VARCHAR(255) NOT NULL,
   `release_date` DATETIME NOT NULL,
-  `artist_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_album_artist1_idx` (`artist_id` ASC) VISIBLE,
-  CONSTRAINT `fk_album_artist1`
-    FOREIGN KEY (`artist_id`)
-    REFERENCES `mydb`.`artist` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`album_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`song`
+-- Table `Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`song` ;
+DROP TABLE IF EXISTS `Songs` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`song` (
-  `id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `Songs` (
+  `song_id` INT NOT NULL,
   `title` VARCHAR(255) NOT NULL,
   `length` TIME NOT NULL,
   `release_date` DATETIME NOT NULL,
-  `album_id` INT NOT NULL,
-  `artist_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
+  `album_id` INT NULL,
+  PRIMARY KEY (`song_id`),
   INDEX `fk_song_album1_idx` (`album_id` ASC) VISIBLE,
-  INDEX `fk_song_artist1_idx` (`artist_id` ASC) VISIBLE,
   CONSTRAINT `fk_song_album1`
     FOREIGN KEY (`album_id`)
-    REFERENCES `mydb`.`album` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_song_artist1`
-    FOREIGN KEY (`artist_id`)
-    REFERENCES `mydb`.`artist` (`id`)
+    REFERENCES `Albums` (`album_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`playlist_song`
+-- Table `Playlists_Songs`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`playlist_song` ;
+DROP TABLE IF EXISTS `Playlists_Songs` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`playlist_song` (
-  `id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `Playlists_Songs` (
+  `playlist_song_id` INT NOT NULL,
   `playlist_id` INT NOT NULL,
   `song_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`playlist_song_id`),
   INDEX `fk_playlist_song_playlist1_idx` (`playlist_id` ASC) VISIBLE,
   INDEX `fk_playlist_song_song1_idx` (`song_id` ASC) VISIBLE,
   CONSTRAINT `fk_playlist_song_playlist1`
     FOREIGN KEY (`playlist_id`)
-    REFERENCES `mydb`.`playlist` (`id`)
+    REFERENCES `Playlists` (`playlist_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_playlist_song_song1`
     FOREIGN KEY (`song_id`)
-    REFERENCES `mydb`.`song` (`id`)
+    REFERENCES `Songs` (`song_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Artists`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Artists` ;
+
+CREATE TABLE IF NOT EXISTS `Artists` (
+  `artist_id` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `bio` VARCHAR(255) NOT NULL,
+  `genre` VARCHAR(255) NOT NULL,
+  `label` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`artist_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Artists_Songs`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Artists_Songs` ;
+
+CREATE TABLE IF NOT EXISTS `Artists_Songs` (
+  `artist_song_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `artist_id` INT NOT NULL,
+  `song_id` INT NOT NULL,
+  PRIMARY KEY (`artist_song_id`),
+  INDEX `fk_Artists_Songs_Artists1_idx` (`artist_id` ASC) VISIBLE,
+  INDEX `fk_Artists_Songs_Songs1_idx` (`song_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Artists_Songs_Artists1`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `Artists` (`artist_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Artists_Songs_Songs1`
+    FOREIGN KEY (`song_id`)
+    REFERENCES `Songs` (`song_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Artists_Albums`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Artists_Albums` ;
+
+CREATE TABLE IF NOT EXISTS `Artists_Albums` (
+  `artist_album_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `artist_id` INT NOT NULL,
+  `album_id` INT NOT NULL,
+  PRIMARY KEY (`artist_album_id`),
+  INDEX `fk_Artists_Albums_Artists1_idx` (`artist_id` ASC) VISIBLE,
+  INDEX `fk_Artists_Albums_Albums1_idx` (`album_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Artists_Albums_Artists1`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `Artists` (`artist_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Artists_Albums_Albums1`
+    FOREIGN KEY (`album_id`)
+    REFERENCES `Albums` (`album_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -143,5 +167,3 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
