2e545cb0c743849a9097b0257190affeae138d82
Jacob Moore <mooreja2@flip2.engr.oregonstate.edu>
2022-04-27 16:47:12 -0700

    Added example values and tested to see they work

--- DDL.sql
+++ DDL.sql
@@ -26,7 +26,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Playlists` ;
 
 CREATE TABLE IF NOT EXISTS `Playlists` (
-  `playlist_id` INT NOT NULL,
+  `playlist_id` INT NOT NULL AUTO_INCREMENT,
   `created_date` DATETIME NOT NULL,
   `playlist_name` VARCHAR(2555) NOT NULL,
   `user_id` INT NOT NULL,
@@ -46,7 +46,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Albums` ;
 
 CREATE TABLE IF NOT EXISTS `Albums` (
-  `album_id` INT NOT NULL,
+  `album_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `release_date` DATETIME NOT NULL,
   PRIMARY KEY (`album_id`))
@@ -59,7 +59,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Songs` ;
 
 CREATE TABLE IF NOT EXISTS `Songs` (
-  `song_id` INT NOT NULL,
+  `song_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `length` TIME NOT NULL,
   `release_date` DATETIME NOT NULL,
@@ -80,7 +80,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Playlists_Songs` ;
 
 CREATE TABLE IF NOT EXISTS `Playlists_Songs` (
-  `playlist_song_id` INT NOT NULL,
+  `playlist_song_id` INT NOT NULL AUTO_INCREMENT,
   `playlist_id` INT NOT NULL,
   `song_id` INT NOT NULL,
   PRIMARY KEY (`playlist_song_id`),
@@ -105,7 +105,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Artists` ;
 
 CREATE TABLE IF NOT EXISTS `Artists` (
-  `artist_id` INT NOT NULL,
+  `artist_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `bio` VARCHAR(255) NOT NULL,
   `genre` VARCHAR(255) NOT NULL,
@@ -162,6 +162,204 @@ CREATE TABLE IF NOT EXISTS `Artists_Albums` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- EXAMPLE VALUES
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Insert into 'Users'
+-- -----------------------------------------------------
+INSERT INTO `Users` (`first_name`, `last_name`, `email`, `date_joined`, `password`)
+VALUES
+(
+  'Dejounte', 'Murray', 'dj@example.com', '2018-11-20', 'ThisIsMyPassword5'
+),
+(
+  'Leonard', 'Moore', 'leonard@example.com', '2022-01-01', 'IsADog'
+),
+(
+  'Brad', 'Pitt', 'brad@example.com', '2020-05-30', 'MattDamon1!'
+),
+(
+  'Tom', 'Morello', 'tmo@example.com', '2019-02-04', 'Gu3rri11aRadi0'
+),
+(
+  'Robert', 'Bobbie', 'robob@example.com', '2021-07-04', 'BobbyRob!!'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Playlists'
+-- -----------------------------------------------------
+INSERT INTO `Playlists` (`created_date`, `playlist_name`, `user_id`)
+VALUES 
+(
+  '2022-10-03', 'Money Ball', '3'
+),
+(
+  '2022-05-06', 'Sleep Music', '5'
+),
+(
+  '2021-02-02', "Rollin' Down Rodeo", '4'
+),
+(
+  '2021-02-01', 'Simple Plan Entire Discography', '4'
+),
+(
+  '2021-01-05', 'Songs for Dogs', '2'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Playlists_Songs'
+-- -----------------------------------------------------
+INSERT INTO `Playlists_Songs` (`song_id`, `playlist_id`)
+VALUES 
+(
+  '3', '5'
+),
+(
+  '4', '1'
+),
+(
+  '1', '4'
+),
+(
+  '3', '2'
+),
+(
+  '2', '3'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Songs'
+-- -----------------------------------------------------
+INSERT INTO `Songs` (`title`, `length`, `release_date`, `album_id`)
+VALUES 
+(
+  'Generation', '00:03:03', '2008-02-12', '3'
+),
+(
+  'Your Love Is a Lie', '00:03:42', '2008-02-12', '3'
+),
+(
+  'Fireflies', '00:03:48', '2009-07-14', '1'
+),
+(
+  "Ice Is Workin' It", '00:04:36', '1990-09-03', '2'
+),
+(
+  'Walk This Way', '00:03:38', '1986-07-04', '4'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Artists_Songs'
+-- -----------------------------------------------------
+INSERT INTO `Artists_Songs` (`artist_id`, `song_id`)
+VALUES
+(
+  '1', '1'
+),
+(
+  '1', '2'
+),
+(
+  '2', '3'
+),
+(
+  '3', '4'
+),
+(
+  '4', '5'
+),
+(
+  '5', '5'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Albums'
+-- -----------------------------------------------------
+INSERT INTO `Albums` (`title`, `release_date`)
+VALUES
+(
+  'Ocean Eyes', '2009-07-28'
+),
+(
+  'To the Extreme', '1990-09-03'
+),
+(
+  'Simple Plan', '2008-02-12'
+),
+(
+  'Raising Hell', '1996-07-04'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Artists_Albums'
+-- -----------------------------------------------------
+INSERT INTO `Artists_Albums` (`artist_id`, `album_id`)
+VALUES 
+(
+  '2', '1'
+),
+(
+  '3', '2'
+),
+(
+  '1', '3'
+),
+(
+  '4', '4'
+),
+(
+  '5', '4'
+);
+
+-- -----------------------------------------------------
+-- Insert into 'Artists'
+-- -----------------------------------------------------
+INSERT INTO `Artists` (`name`, `bio`, `genre`, `label`)
+VALUES
+(
+  'Simple Plan', 
+  'Simple Plan is a Canadian rock band from Montreal, Quebec formed in 1999.',
+  'Pop Punk', 'Atlantic'
+),
+(
+  'Owl City', 
+  'Owl City is an American electronic music project created in 2007 in Owatonna, Minnesota.',
+  'Synth Pop', 'Republic'
+),
+(
+  'Vanilla Ice', 
+  'Robert Matthew Van Winkle (born October 31, 1967), known professionally as Vanilla Ice, is an American rapper, actor, and television host.',
+  'Hip Hop', 'SBK'
+),
+(
+  'Run-DMC',
+  'Run-DMC (also spelled Run-D.M.C.) was an American hip hop group from Hollis, Queens, New York City, founded in 1983 by Joseph Simmons, Darryl McDaniels, and Jason Mizell.',
+  'Hip Hop', 'Profile'
+),
+(
+  'Aerosmith', 
+  'Aerosmith is an American rock band formed in Boston in 1970.', 
+  'Hard Rock', 'Columbia'
+);
+
+
+-- -----------------------------------------------------
+-- TEST QUERIES
+-- -----------------------------------------------------
+select * from Users;
+select * from Playlists;
+select * from Playlists_Songs;
+select * from Songs;
+select * from Artists_Songs;
+select * from Artists;
+select * from Artists_Albums;
+select * from Albums;
+-- -----------------------------------------------------
+-- TEST QUERIES
+-- -----------------------------------------------------
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
